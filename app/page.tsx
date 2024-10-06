60584e48378b4ecff430c3d538e91722dba1991d
emir2269 <ali724345@gmail.com>
2024-10-06 18:49:21 +0300

    installed some dependencies. and applied some animation for background color.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <firstPage />
+        <FirstPage />
+        
       </div>
     </main>
   );
