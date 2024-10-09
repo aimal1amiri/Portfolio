f0d2d436ebb519a7ed26e391253c38ae570d9f9d
emir2269 <ali724345@gmail.com>
2024-10-09 18:52:08 +0300

    i add some animation and set up position of dives in firstPage.

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -7,7 +7,7 @@ const Grid = () => {
     <section id="about">
         <BentoGrid>
             {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName, spareImg})=> (
-                <BentoGridItem id={id} key={id} title={title} description={description} className={className} img={img}/>
+                <BentoGridItem id={id} key={id} title={title} description={description} className={className} img={img} imgClassName={imgClassName} titleClassName={titleClassName} spareImg={spareImg}/>
                 
             ))}
         </BentoGrid>
