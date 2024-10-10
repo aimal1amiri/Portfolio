08128bd80bd19bd3351e1dedefeb61985d05b585
emir2269 <ali724345@gmail.com>
2024-10-10 12:13:01 +0300

    Added earth motion to the div. the source code is in -aceternity.io- .

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBackground";
+import { GlobeDemo } from "./GridGlobe";
+
 
 export const BentoGrid = ({
   className,
@@ -45,7 +47,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
         className
       )}
       style={{
@@ -54,7 +56,7 @@ export const BentoGridItem = ({
       }}
     >
       <div className={`${id === 6} && 'flex justify-center h-full` }>
-        <div className="w-full h-full absolute">
+        <div className="w-full h-full absolute ">
           {img &&(
             <img src={img} alt={img} className={cn(imgClassName, 'object-cover, object-center')}/>
 
@@ -78,7 +80,22 @@ export const BentoGridItem = ({
         <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
         </div>
-        </div>         
+        </div> 
+        {id === 2 && <GlobeDemo/>}        
+
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 ">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {['React.js', 'Next.js','Typescript'].map((item)=>(
+                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+            </div>
+
+          </div>
+
+        )}
 
       </div>
       </div>
