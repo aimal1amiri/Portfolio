c0b5bc9a18e758b104a4b2c6f9e2b738a01a9170
emir2269 <ali724345@gmail.com>
2024-10-11 20:43:37 +0300

    now i gonna work on recentProjects file where i can put my projects summary.

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { projects } from '@/data/gridItems'
 
 const RecentProjects = () => {
   return (
-    <div>RecentProjects</div>
+    <div className='py-20'>
+        <h1 className='heading'>
+            A small selection of {''}
+            <span>
+                Recent Projects
+            </span>
+        </h1>
+        <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+            {projects.map(({id, title, des, img, iconLists, link}) => (
+                <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+                    {title}
+            </div>
+            ))} 
+        </div>
+    </div>
   )
 }
 
