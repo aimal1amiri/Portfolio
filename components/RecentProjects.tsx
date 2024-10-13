00e6772ef24c8a593989c15dd23f6b270133c63c
emir2269 <ali724345@gmail.com>
2024-10-13 22:42:45 +0300

    I have created pin motion for the project cards, when the curser hover on it, it goes back and will show the details about the project or it will show you that where it will take you when you click on it

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { projects } from '@/data/gridItems'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -13,8 +14,11 @@ const RecentProjects = () => {
         <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
             {projects.map(({id, title, des, img, iconLists, link}) => (
                 <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
-                    {title}
+                    <PinContainer>
+                        {title}
+                    </PinContainer>
             </div>
+
             ))} 
         </div>
     </div>
